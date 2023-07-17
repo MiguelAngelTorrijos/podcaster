025cf75b55b89d73e576b4a4ef0590c1a335418a
Miguel <mats1346@gmail.com>
2023-07-17 20:24:26 +0200

    translate error codes

--- src/utilities/get-validation-error.js
+++ src/utilities/get-validation-error.js
@@ -1,9 +1,9 @@
 export const getValidationError = errorCode => {
 	const codeMatcher = {
-		ERR_NETWORK: 'Se rompió la red',
-		ERR_TIMEOUT: 'Se acabó el tiempo',
-		ERR_CANCEL: 'Se canceló la petición',
-		ERR_UNKNOWN: 'Error desconocido',
+		ERR_NETWORK: 'Network error',
+		ERR_TIMEOUT: 'Timeout',
+		ERR_CANCEL: 'Request canceled',
+		ERR_UNKNOWN: 'Unknown error',
 		ERR_400: 'Error 400',
 		ERR_401: 'Error 401',
 		ERR_403: 'Error 403',
